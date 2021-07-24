afb587755d447680c3958d482c8959f6e6764427
YamiVeraLopez <yamilaveralopez@gmail.com>
2021-07-24 03:30:29 -0300

    Agrego archivos

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-let express = require('express');
-let path = require('path');
-let app = express();
-let port = 3000;
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, `/views/index.html`) )
+})
+
+app.get('/header', (req, res) => {
+    res.sendFile(path.join(__dirname, `/views/header.html`) )
+})
 
 app.listen(port, () => {
     console.log(`Puerto corriendo en ${port}\n http://localhost:${port}`)
