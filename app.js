44421a724fb743f7faabe58d4b90038ac2c68a91
Maximo <maxiarebalo12@gmail.com>
2021-07-25 02:15:11 -0300

    hago login y register

--- app.js
+++ app.js
@@ -13,6 +13,18 @@ app.get('/header', (req, res) => {
     res.sendFile(path.join(__dirname, `/views/header.html`) )
 })
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, `/views/login.html`) )
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, `/views/register.html`))
+})
+
+app.get('/submit', (req, res) => {
+    res.sendFile(path.join(__dirname, `/views/submit.html`))
+})
+
 app.listen(port, () => {
     console.log(`Puerto corriendo en ${port}\n http://localhost:${port}`)
 })
