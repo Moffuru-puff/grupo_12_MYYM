7b066ce56e931df859f320e0526ed824a662203f
YamiVeraLopez <yamilaveralopez@gmail.com>
2021-08-02 05:19:44 -0300

    footer

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ app.get('/header', (req, res) => {
     res.sendFile(path.join(__dirname, `/views/header.html`) )
 })
 
+app.get('/footer', (req, res) => {
+    res.sendFile(path.join(__dirname, `/views/footer.html`) )
+})
+
 app.listen(port, () => {
     console.log(`Puerto corriendo en ${port}\n http://localhost:${port}`)
 })
