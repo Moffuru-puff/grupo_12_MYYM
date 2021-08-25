66dfc9b7afe215650b278169c883b6d01254cd2b
YamiVeraLopez <yamilaveralopez@gmail.com>
2021-08-25 04:22:10 -0300

    agrego vistas de admin

--- routes/cargaDeProductos.js
+++
@@ -1,9 +0,0 @@
-/* let express = require('express');
-let router = express.Router();
-let controller = require('../controllers/adminController'); */
-
-/* GET  */
-/* router.get('/', controller.charge);
-
-
-module.exports = router; */
