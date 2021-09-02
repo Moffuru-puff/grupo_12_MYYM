77d83eb16983e8b7235bf9b7722d5787821f665b
Maximo <maxiarebalo12@gmail.com>
2021-09-01 23:41:41 -0300

    modifico validaciones

--- validations/registerValidator.js
+++ validations/registerValidator.js
@@ -1,22 +1,22 @@
-let { check } = require('express-validator');
+let { check, body } = require('express-validator');
 
 module.exports = [
     check('usuario')
     .notEmpty()
-    .withMessage("Debe ingresar un nombre de usuario")
+    .withMessage("Debe ingresar un nombre de usuario").bail()
     .isLength({min: 5})
     .withMessage("Ingrese un usuario minimo de 5 caracteres"),
 
     check('email')
     .notEmpty()
-    .withMessage("Debe ingresar su email")
+    .withMessage("Debe ingresar su email").bail()
     .isEmail()
     .withMessage("Debe ingresar un email valido")
     ,
     
     check('password')
     .notEmpty()
-    .withMessage('Debe ingresar una contraseña')
+    .withMessage('Debe ingresar una contraseña').bail()
     .isLength({min: 4, max: 10})
     .withMessage('Debe ingresar una contraseña de 4-10 caracteres')
   /*   .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d@$.!%*#?&]/,)
@@ -24,10 +24,13 @@ module.exports = [
     /* .equals('password', 'repassword') */
     /* .withMessage('Las contraseñas no coinciden'), */
 
+    body('repassword')
+    .custom((value, {req}) => value !== req.body.password ? false : true)
+    .withMessage("Las contraseñas no coinciden"),
 
-    check('repassword')
+    /*check('repassword')
     .notEmpty()
-    .withMessage('Debe re-ingresar la contraseña')/* 
+    .withMessage('Debe re-ingresar la contraseña') 
     .equals('password', 'repassword')
     .withMessage('Las contraseñas no coinciden') */
     
