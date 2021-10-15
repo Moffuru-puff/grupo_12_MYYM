52018aa68d3e152ac60eb215c0295c3c7b1e7ed4
Maximo <maxiarebalo12@gmail.com>
2021-10-15 14:07:12 -0300

    agrego validaciones

--- src/routes/users.js
+++ src/routes/users.js
@@ -12,24 +12,23 @@ const {
     } = require('../controllers/usersController');
 const reValidator = require('../validations/registerValidator')
 const loValidator = require('../validations/loginValidator')
-const editProValidator = require('../validations/editProfileValidator')
 const uploadUserAvatar = require('../middlewares/uploadUserAvatar')
 const sessionCheck = require('../middlewares/sessionCheck')
 const userLoginCheck = require('../middlewares/userLoginCheck')
 
 /* GET - login */
-router.get('/login', /* userLoginCheck , */login)
-router.post('/login', /* loValidator , */loginUser)
-router.get('/logout', /* sessionCheck,  */logout)
+router.get('/login', userLoginCheck ,login)
+router.post('/login', loValidator ,loginUser)
+router.get('/logout', sessionCheck, logout)
 
 /* GET - Register */
-router.get('/register', /* userLoginCheck , */ register)
-router.post('/register', /* reValidator ,*/ registerNewUser)
+router.get('/register', userLoginCheck , register)
+router.post('/register', reValidator , registerNewUser)
 
 
 /* GET - Profile */
-router.get('/profile', /* sessionCheck , */profile)
-router.get('/profile/editprofile/:id', /* sessionCheck , */editProfile)
+router.get('/profile', sessionCheck ,profile)
+router.get('/profile/editprofile/:id', sessionCheck ,editProfile)
 router.put('/profile/editprofile/:id' ,uploadUserAvatar.single('avatar'), updateProfile)
 
 
